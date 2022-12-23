253c692c424f88124607bbd850b3bec66e396676
Sam nithin Chockcee <samnithinchockcee@Sams-MacBook-Pro.local>
2022-12-23 12:35:26 +0530

    Added a java file
    Min Max values

---
+++ sda.java
@@ -0,0 +1,10 @@
+public class sda {
+    public static void main(String[] args)
+    {
+        System.out.println("Int min "+Integer.MIN_VALUE);
+        System.out.println("Int max "+Integer.MAX_VALUE);
+        System.out.println("Int bytes "+Integer.BYTES);
+
+
+    }
+}
